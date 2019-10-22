289acdae883e5ccd8a09b8468007b414fe5f3d08
Gilberto Hernández <joseg.hernandez@udem.edu>
2019-10-22 10:28:32 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Telepromter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
