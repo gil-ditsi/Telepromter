61866d500ab85c8731717d02fa37e2430a491848
Gilberto Hernández <joseg.hernandez@udem.edu>
2019-10-22 10:35:36 -0500

    Etapa 1 completa
    Co-authored-by: JEMS93 <eduardo.mendoza@neoris.com>
    Co-authored-by: hlozanor <hlozanor@hotmail.com>

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Telepromter
 {
@@ -6,7 +8,25 @@ namespace Telepromter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            var lines = ReadFrom("sampleQuotes.txt");
+            foreach (var item in lines)
+            {
+                Console.WriteLine(item);
+            }
+
         }
+
+        static IEnumerable<string> ReadFrom(string file){
+            string line;
+            using (StreamReader reader = File.OpenText(file))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
+        }
+
     }
 }
